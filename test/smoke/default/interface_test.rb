2c7e8de34cf8740a74dc0d10e4e0d260dd4ff1fd
Jonathan Hartman <j@p4nt5.com>
2017-06-10 13:35:16 -0700

    Build up some more thorough Inspec tests

---
+++ test/smoke/default/interface_test.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+describe command('ethtool eth1') do
+  it 'indicates eth1 is up' do
+    expect(subject.exit_status).to eq(0)
+  end
+end
+
+describe command('ip a') do
+  it 'shows eth1 has grabbed an IP address' do
+    r = Regexp.new('^ +inet [0-9]+(\.[0-9]+){3}/[0-9]+ brd ' \
+                   '[0-9]+(\.[0-9]+){3} scope global eth1$')
+    expect(subject.stdout).to match(r)
+  end
+end
+
+describe file('/etc/dhcp/dhclient.conf') do
+  it 'makes eth1 use broadcast for its DHCP requests' do
+    r = Regexp.new('^interface "eth1" { supersede dhcp-server-identifier ' \
+                   '255\.255\.255\.255; }$')
+    expect(subject.content).to match(r)
+  end
+end
---
+++ test/smoke/default/routing_test.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+describe command('ip route show') do
+  it 'includes the normal route for eth1' do
+    r = Regexp.new('^[0-9]+(\.[0-9]+){3}/[0-9]+ dev eth1  proto kernel  ' \
+                   'scope link  src [0-9]+(\.[0-9]+){3} $')
+    expect(subject.stdout).to match(r)
+  end
+
+  it 'includes the extra metric route for eth1' do
+    r = /^default via [0-9]+(\.[0-9]+){3} dev eth1  metric 10001 $/
+    expect(subject.stdout).to match(r)
+  end
+end
+
+describe command('ip route show table 10001') do
+  it 'has a default route out eth1' do
+    r = /^default via [0-9]+(\.[0-9]+){3} dev eth1 $/
+    expect(subject.stdout).to match(r)
+  end
+end
+
+describe command('curl --interface eth1 --connect-timeout 3 ' \
+                 'https://www.google.com') do
+  it 'indicates a successful route out of eth1' do
+    expect(subject.exit_status).to eq(0)
+  end
+end
